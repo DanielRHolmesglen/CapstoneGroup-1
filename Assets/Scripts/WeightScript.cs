d9fd89da4cf1655dd941922ae0f667e269733f87
ZedFox <62165771+ZedFox@users.noreply.github.com>
2021-08-20 16:30:38 +1000

    Testing

---
+++ Assets/Scripts/WeightScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class WeightScript : MonoBehaviour
+{
+    AudioSource weightAudio;
+    public KeyCode Button;
+    bool isPlaying = false;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        weightAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            weightAudio.volume = 1;
+        }
+        else
+        {
+            weightAudio.volume = 0;
+        }
+        if (Input.GetKeyDown(Button))
+        {
+            isPlaying = !isPlaying;
+        }
+    }
+}
