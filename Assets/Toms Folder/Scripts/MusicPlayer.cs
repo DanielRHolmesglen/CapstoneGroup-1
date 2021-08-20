87a3fb6db4963e6d3fd53fb8081d15e088377bbb
Bomtill <80375642+Bomtill@users.noreply.github.com>
2021-08-20 16:39:12 +1000

    Weekly update
    added prefabs and basic scripts

---
+++ Assets/Toms Folder/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicPlayer : MonoBehaviour
+{
+    //[SerializeField] AudioMixer mixer;
+
+    //private AudioSource musicPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
