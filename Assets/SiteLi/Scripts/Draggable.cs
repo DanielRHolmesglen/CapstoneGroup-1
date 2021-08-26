68968844fc0e610559ff7dd412a995ef32c26987
SiTeLi23 <87678896+SiTeLi23@users.noreply.github.com>
2021-08-26 17:47:12 +1000

    Scale function implement
    adding scripts for weight,game manager, scale,so the scale can detect total weight value of both side and check if it's eaqul

---
+++ Assets/SiteLi/Scripts/Draggable.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+
+
+    //ps: this script can be attached to whatever you want to drag with mouse cursor
+    // well , i had to admit it behave pretty bad right now and I got no idea how to improve
+
+
+
+    private Vector3 mOffset;
+
+    private float mZCoord;
+
+    public bool isDragging = false;
+
+    Rigidbody rb;
+    
+
+
+
+    private void Start()
+    {
+       var rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+        //store offset ,so the position of cursor and selected object can be relatively smooth?
+        //well at least that's what I understand from the tutotial.....
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+
+     Vector3 GetMouseWorldPos()
+    {
+        //pixel coordinates of mouse(x,y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        //z cordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        //convert it to world points
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+
+
+    }
+
+    //when pressing down the mouse , the selected object will follow the cursor
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+        
+        
+        isDragging = true;
+    }
+
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+
+
+    
+
+
+}
