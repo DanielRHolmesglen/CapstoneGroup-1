52ef3727a373d9f18659cd1da35f60c83e8473d7
Bomtill <80375642+Bomtill@users.noreply.github.com>
2021-09-02 15:01:37 +1000

    Update to tilting class

--- Assets/Toms Folder/Scripts/ScaleTilter.cs
+++ Assets/Toms Folder/Scripts/ScaleTilter.cs
@@ -16,8 +16,9 @@ public class ScaleTilter : MonoBehaviour
     private float weightSmall = 2f;
     [SerializeField] private float tiltSpeed = 1f;
 
-    private Quaternion currentTiltAngle;
+    private Quaternion startTiltAngle;
 
+    // Scale balance points
     private Quaternion maxLeft = Quaternion.Euler(0,0,15);
     private Quaternion closeLeft = Quaternion.Euler(0, 0, 5);
     private Quaternion balanced = Quaternion.Euler(0, 0, 0);
@@ -27,47 +28,77 @@ public class ScaleTilter : MonoBehaviour
     void Start()
     {
         crossBar = GetComponent<GameObject>();
-        currentTiltAngle = Quaternion.Euler(0, 0, 10);
-        transform.rotation = Quaternion.Lerp(currentTiltAngle,currentTiltAngle, Time.time * tiltSpeed);
+        startTiltAngle = Quaternion.Euler(0, 0, 10); 
+        transform.rotation = Quaternion.Lerp(transform.rotation,startTiltAngle, Time.deltaTime * tiltSpeed);
     }
 
     
-    void Update() // this should only be checked when a wheight is placed. **need to fix**
+    void Update() // this should only be checked when a weight is placed. **need to fix**
     {
         LargeWeightAdded();
         MediumWeightAdded();
         SmallWeightAdded();
 
+
+        #region Weight Added logic // commented out
+        /*
         if (leftPlateValue > rightPlateValue)
         {
-            if (leftPlateValue  <= (rightPlateValue + 3)) // need to check if close not greater or equal to
+            if (leftPlateValue  <= (rightPlateValue + 3)) 
             {
-                transform.rotation = Quaternion.Lerp(currentTiltAngle, closeLeft,Time.deltaTime * tiltSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, closeLeft,Time.deltaTime * tiltSpeed);
                 return;
             }
-            transform.rotation = Quaternion.Lerp(currentTiltAngle, maxLeft, Time.deltaTime * tiltSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, maxLeft, Time.deltaTime * tiltSpeed);
         } 
         else if (rightPlateValue > leftPlateValue)
         {
             if (rightPlateValue <= (leftPlateValue + 3))
             {
-                transform.rotation = Quaternion.Lerp(currentTiltAngle, closeRight, Time.deltaTime * tiltSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, closeRight, Time.deltaTime * tiltSpeed);
                 return;
             }
-            transform.rotation = Quaternion.Lerp(currentTiltAngle, maxRight, Time.deltaTime * tiltSpeed); 
+            transform.rotation = Quaternion.Lerp(transform.rotation, maxRight, Time.deltaTime * tiltSpeed);
         } 
-        else if (rightPlate == leftPlate)
+        else if (leftPlateValue == rightPlateValue)
         {
-            transform.rotation = Quaternion.Lerp(currentTiltAngle, balanced, Time.deltaTime * tiltSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, balanced, Time.deltaTime * tiltSpeed);
             //Do balanced effects.
             return;
         }
-        
+        */
+        #endregion 
     }
+    private void WeightAdded() //  IEnumerator?
+    {
+        if (leftPlateValue > rightPlateValue)
+        {
+            if (leftPlateValue <= (rightPlateValue + 3))
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, closeLeft, Time.deltaTime * tiltSpeed);
+                return;
+            }
+            transform.rotation = Quaternion.Lerp(transform.rotation, maxLeft, Time.deltaTime * tiltSpeed);
+        } else if (rightPlateValue > leftPlateValue)
+        {
+            if (rightPlateValue <= (leftPlateValue + 3))
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, closeRight, Time.deltaTime * tiltSpeed);
+                return;
+            }
+            transform.rotation = Quaternion.Lerp(transform.rotation, maxRight, Time.deltaTime * tiltSpeed);
+        } else if (leftPlateValue == rightPlateValue)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, balanced, Time.deltaTime * tiltSpeed);
+            //Do balanced effects.
+            return;
+        }
+    }
+
     #region WeightInputs
     // disregard below it's only to test the code, 
     // this will all be set by putting the weights on the plates eventually
-    public void LargeWeightAdded()
+    public void LargeWeightAdded() // can add code in here to make plates move.
     {
         if (Input.GetKeyDown(KeyCode.Q)) leftPlateValue += weightLarge;
         else if (Input.GetKeyDown(KeyCode.E)) rightPlateValue += weightLarge;
