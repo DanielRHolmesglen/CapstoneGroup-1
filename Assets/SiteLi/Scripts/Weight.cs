c03f9e8d44602c5f66914a2ef3e82c1b5ef6f109
Bomtill <80375642+Bomtill@users.noreply.github.com>
2021-09-10 10:31:07 +1000

    update

--- Assets/SiteLi/Scripts/Weight.cs
+++ Assets/SiteLi/Scripts/Weight.cs
@@ -7,24 +7,42 @@ public class Weight : MonoBehaviour
     //ps: this script need to be attached to objects that you want to give it a value(weight , Goal_object)
 
     //basically just to store the value of each weight that attached with this script
-    public float value;
+    public float weightValue;
     public AudioSource soundEffect;
-   
+    public GameObject visualEffect;
 
-    
+    private void Start()
+    {
+        if (gameObject.GetComponent<AudioSource>())
+        {
+            soundEffect = gameObject.GetComponent<AudioSource>();
+            soundEffect.volume = 0;
+        }
+    }
+
     public float GetValue()
     {
 
-        return value;
+        return weightValue;
     }
 
 
     public void PlaySound()
     {
-
-        soundEffect.Play();
+        soundEffect.volume = 1;
+        if (visualEffect)
+        {
+            visualEffect.GetComponent<ParticleSystem>().Play();
+        }
     }
-
+    public void StopPlaySound()
+    {
+        soundEffect.volume = 0;
+        if (visualEffect)
+        {
+            visualEffect.GetComponent<ParticleSystem>().Stop();
+        }
+    }
 
 
 }
